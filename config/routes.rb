4a4661ebe6fc1d06ff4c53eb08097a403e53c11b
BPluribus81 <ender081@gmail.com>
2013-03-18 10:30:45 -0400

    Get the first basic static pages working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Pluribus::Application.routes.draw do
   get "static_pages/home"
-
+  get "static_pages/about"
   get "static_pages/help"
 
   # The priority is based upon order of creation:
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
+require 'capybara/rspec'
 require 'rspec/rails'
 require 'rspec/autorun'
 
@@ -16,6 +17,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.include Capybara::DSL
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
